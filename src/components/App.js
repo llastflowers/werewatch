99cfd446e8436fd42e0c5eb179a34b58bb76b7e1
Brittany Houtz <brittany.houtz@gmail.com>
2020-06-12 13:53:02 -0700

    app.js setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './Home';
+
+const Header = () => (
+  <header>
+    <Link to="/">
+      <h1>WereWatch</h1>
+    </Link>
+  </header>
+);
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Header />
+      <Route exact path="/" component={Home}/>
+    </Router>
+  );
 }
-  
