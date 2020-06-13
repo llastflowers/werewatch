cc8b0076783e21c1d3358a4f9d63bbbd9d09f993
Brittany Houtz <brittany.houtz@gmail.com>
2020-06-12 18:59:43 -0700

    starting to work on first hook

--- src/services/movieApis.js
+++ src/services/movieApis.js
@@ -1,9 +1,9 @@
-export const getMovies = (query) => {
-  return fetch('')
-    .then(res => res.json());
-};
+// export const getMovies = (query) => {
+//   return fetch('')
+//     .then(res => res.json());
+// };
 
-export const getProviders = (movieid) => {
-  return fetch('')
-    .then(res => res.json());
-};
+// export const getProviders = (movieid) => {
+//   return fetch('')
+//     .then(res => res.json());
+// };
