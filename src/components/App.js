171056170c9845c3f67011bd3bc459b779dde3fc
Brittany Houtz <brittany.houtz@gmail.com>
2020-06-12 19:10:38 -0700

    added footer and simplified homepage links

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 import Home from './Home';
 
 const Header = () => (
   <header>
-    <Link to="/">
+    <a target="_blank" href="/">
       <h1>WereWatch</h1>
-    </Link>
+    </a>
   </header>
+);
+
+const Footer = () => (
+  <footer>
+    <a target="_blank" rel="noopener noreferrer" href="https://github.com/llastflowers/werewatch">
+      <p>GitHub</p>
+    </a>
+  </footer>
 );
 
 export default function App() {
@@ -19,6 +26,7 @@ export default function App() {
     <Router>
       <Header />
       <Route exact path="/" component={Home}/>
+      <Footer />
     </Router>
   );
 }
