72c09def0e191001a58d9d653721d76b4e2c4f2f
Brittany Houtz <brittany.houtz@gmail.com>
2020-06-14 14:54:52 -0700

    set up custom hook for getting movie search results

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,1 +1,19 @@
 
+import { useState, useEffect } from 'react';
+import { getMovies } from '../services/movieApis';
+
+export const useMovies = (query) => {
+  const [movieResults, setMovieResults] = useState({
+    movies:[]
+  });
+  useEffect(() => {
+    if(query !== '') {
+
+      getMovies(query)
+        .then(movieResults => setMovieResults(movieResults));
+    }
+  }, [query]);
+  return movieResults;
+};
+
+export default useMovies;
