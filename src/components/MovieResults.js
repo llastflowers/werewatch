da88b362b5cbd379a99c11d5f4b18761ff15bd1e
Brittany Houtz <brittany.houtz@gmail.com>
2020-06-14 19:54:35 -0700

    use handlesubmit to update query

--- src/components/MovieResults.js
+++ src/components/MovieResults.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import PropTypes from 'prop-types';
 
-const MovieResults = ({ moviesList }) => {
-  const movieItem = moviesList.results.map(movie => (
-    <li key={movie.id}>
-      <p>{movie.title}</p>
-    </li>
-  ));
+// const MovieResults = ({ moviesList }) => {
+//   const movieItem = moviesList.results.map(movie => (
+//     <li key={movie.id}>
+//       <p>{movie.title}</p>
+//     </li>
+//   ));
 
-  return (
-    <>
-      <ul>
-        {movieItem}
-      </ul>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <ul>
+//         {movieItem}
+//       </ul>
+//     </>
+//   );
+// };
 
-MovieResults.propTypes = {
-  moviesList: PropTypes.array
-};
+// MovieResults.propTypes = {
+//   moviesList: PropTypes.array
+// };
 
-export default MovieResults;
+// export default MovieResults;
--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
 import useMovies from '../hooks/useMovies';
 // import MovieResults from './MovieResults';
-
 const MovieSearch = () => {
   const [query, setQuery] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const moviesList = useMovies(query);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setQuery(searchTerm);
+  };
+  
   const searchResults = moviesList.results.map(movie =>
     <li key={movie.id}>
       <p>{movie.title}</p>
     </li>
+    
   );
-
   return (
     <>
-      <form>
-        <input type='text' placeholder="Search by movie title" value={query} onChange={(event) => setQuery(event.target.value)} />
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder="Search by movie title" value={searchTerm} onChange={({ target }) => setSearchTerm(target.value)} />
         <button>Search</button>
       </form>
       <ul>
@@ -23,5 +29,4 @@ const MovieSearch = () => {
     </>
   );
 };
-
 export default MovieSearch;
