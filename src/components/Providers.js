f5fe2fd6deda19fe5edf4d1843a7568ada9c5364
Brittany Houtz <brittany.houtz@gmail.com>
2020-06-15 19:41:06 -0700

    where-to-watch icons are links

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -6,7 +6,9 @@ const Providers = ({ match }) => {
   const providers = useProviders(match.params.movie_id);
   const movieProviders = providers.collection.locations.map(provider => (
     <li key={provider.id}>
-      <p>{provider.display_name}</p>
+      <a target="_blank" rel="noopener noreferrer" href={provider.url}>
+        <img src={provider.icon} style={{ height: '20px' }} onError={(e)=>{e.target.onerror = null; e.target.src = 'https://dimensionmill.org/wp-content/uploads/2019/03/square-placeholder.jpg';}} />
+      </a>
     </li>
   ));
 
