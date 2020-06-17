77aec3db25e42e53e7096be5635968845faa8e63
Brittany Houtz <brittany.houtz@gmail.com>
2020-06-16 18:10:17 -0700

    styled detail page and some other style tweaks

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { getDetails } from '../services/movieApis';
+import styles from './MovieDetail.css';
 
 const MovieDetail = ({ match }) => {
   const [details, setDetails] = useState({});
@@ -11,11 +12,15 @@ const MovieDetail = ({ match }) => {
 
   return (
     <>
-      <img src={`http://image.tmdb.org/t/p/w780/${details.poster_path}`} style={{ width: '100px' }} onError={(e)=>{e.target.onerror = null; e.target.src = 'https://dimensionmill.org/wp-content/uploads/2019/03/square-placeholder.jpg';}} />
-      <p>{details.title}</p>
-      <p>{details.release_date}</p>
-      <p>{details.runtime} minutes</p>
-      <p>{details.overview}</p>
+      <div className={styles.movieItem}>
+        <img className={styles.moviePoster} src={`http://image.tmdb.org/t/p/w780/${details.poster_path}`} onError={(e)=>{e.target.onerror = null; e.target.src = 'https://lh3.googleusercontent.com/proxy/GOuPCAzhMBl60T5N9_oNmDUCRRh8kdI0QukUzA4YvACAlP9i0CniYBeh0FuKlNvXBI_0QA6l-GlvgpczcEZ2n0yN3Dpf_xzqsT39iXs3pw4njq15g34_aQAv-Pt5DsgG03Wg';}} />
+        <div className={styles.movieDetails}>
+          <p className={styles.movieTitle}>{details.title}</p>
+          <p className={styles.movieDate}>{details.release_date?.slice(0, 4)}</p>
+          <p className={styles.movieTime}>{details.runtime} minutes</p>
+          <p className={styles.movieOverview}>{details.overview}</p>
+        </div>
+      </div>
     </>
   );
 };
