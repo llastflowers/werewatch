b65032a6e4d941c0dc132b4ad63534dd7c89c333
Brittany Houtz <brittany.houtz@gmail.com>
2020-06-17 16:01:14 -0700

    local image files

--- src/components/Providers.js
+++ src/components/Providers.js
@@ -9,7 +9,7 @@ const Providers = ({ match }) => {
     <li key={provider.id}>
       <div  className={styles.providers}>
         <a target="_blank" rel="noopener noreferrer" href={provider.url}>
-          <img className={styles.providerIcon} src={provider.icon} onError={(e)=>{e.target.onerror = null; e.target.src = 'https://images.vexels.com/media/users/3/135175/isolated/preview/730a8de14e2d462e3abaad6d99116c87-play-button-circle-icon-by-vexels.png';}} />
+          <img className={styles.providerIcon} src={provider.icon} onError={(e)=>{e.target.onerror = null; e.target.src = 'src/assets/provider_placeholder.png';}} />
         </a>
       </div>
     </li>
