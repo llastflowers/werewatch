fdc66c9ae252098619d6d22168a1e2cd87cdcfb8
Brittany Houtz <brittany.houtz@gmail.com>
2020-08-15 18:48:02 -0700

    refactoring

--- src/services/movieApis.js
+++ src/services/movieApis.js
@@ -11,12 +11,13 @@ export const getDetails = (movie_id) => {
 };
 
 export const getProviders = (movie_id) => {
-  return fetch(`https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=US&source_id=${movie_id}&source=tmdb`, {
+  return fetch(`https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?country=us&source_id=${movie_id}&source=tmdb`, {
+    'method': 'GET',
     'headers': {
+      'x-rapidapi-host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com',
       // eslint-disable-next-line
       'x-rapidapi-key': `${process.env.UTELLY_API_KEY}`
     }
   })
     .then(res => res.json());
-  // .then(res => console.log(res));
 };
